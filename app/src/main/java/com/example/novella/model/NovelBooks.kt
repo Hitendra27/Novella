be9ae880b2d7b3ce36b0c5e35b4b4f2647dc794d
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-08-26 13:18:29 +0100

    feat: data class NovelBooks.kt

---
+++ app/src/main/java/com/example/novella/model/NovelBooks.kt
@@ -0,0 +1,9 @@
+package com.example.novella.model
+
+data class NovelBooks(
+    val id: Int,
+    val novelName: Int,
+    val novelAuthor: Int,
+    val novelDateOfPublication: Int,
+    val ibncNumber: Int
+)
